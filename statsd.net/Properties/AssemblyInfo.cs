a3832546c3afce82c7f607bf0df46799366cd359
Luke Venediger <lukev@lukev.net>
2014-02-18 22:22:47 +0200

    Bumped version number to v.1.4.0.4

--- statsd.net/Properties/AssemblyInfo.cs
+++ statsd.net/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 // Uses SEMVER: http://semver.org/
-[assembly: AssemblyVersion("1.4.0.3")]
-[assembly: AssemblyFileVersion("1.4.0.3")]
+[assembly: AssemblyVersion("1.4.0.4")]
+[assembly: AssemblyFileVersion("1.4.0.4")]
