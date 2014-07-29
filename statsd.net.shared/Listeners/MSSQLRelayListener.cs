30643e33ec78e9247eb51d712e6b8beb3e41d5ed
Luke Venediger <lukev@lukev.net>
2014-07-29 16:34:47 +0200

    Start of the changes to add a db metric relay feature to statsd.net

---
+++ statsd.net.shared/Listeners/MSSQLRelayListener.cs
@@ -0,0 +1,58 @@
+﻿using statsd.net.shared.Services;
+using statsd.net.shared.Structures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace statsd.net.shared.Listeners
+{
+    public class MSSQLRelayListener : IListener
+    {
+        private static string[] SPACE_SPLITTER = new String[] { " " };
+
+        private string _connectionString;
+        private IIntervalService _intervalService;
+        private int _batchSize;
+        private bool _deleteAfterSend;
+
+        private BufferBlock<RelayMetric> _buffer;
+        private ActionBlock<RelayMetric[]> _feeder;
+        private ITargetBlock<string> _target;
+
+        public MSSQLRelayListener(string connectionString, 
+            IIntervalService intervalService,
+            int batchSize, 
+            bool deleteAfterSend) 
+        {
+            _connectionString = connectionString;
+            _intervalService = intervalService;
+            _batchSize = batchSize;
+            _deleteAfterSend = deleteAfterSend;
+            _buffer = new BufferBlock<RelayMetric>();
+            _feeder = new ActionBlock<RelayMetric[]>(p => FeedMetrics(p));
+        }
+
+        private Task FeedMetrics(RelayMetric[] metrics)
+        {
+            foreach(RelayMetric metric in metrics)
+            {
+                var parts = line.Split(SPACE_SPLITTER, StringSplitOptions.RemoveEmptyEntries);
+                _target.Post(parts[0] + ":" + parts[1] + "|r|" + parts[2]);
+            }
+        }
+
+        public void LinkTo(ITargetBlock<string> target, CancellationToken token)
+        {
+            _target = target;
+        }
+
+        public bool IsListening
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
